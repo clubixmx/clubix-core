7e575bf354488a8ee36b19f9d2a0a41524516407
Jonathan Gomez <jonakmex@gmail.com>
2026-03-21 09:26:30 -0600

    feat: initial bdd passing tests

---
+++ src/test/java/com/clubix/steps/ExampleSteps.java
@@ -0,0 +1,24 @@
+package com.clubix.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ExampleSteps {
+
+    @Given("I have a precondition")
+    public void iHaveAPrecondition() {
+        // TODO: implement
+    }
+
+    @When("I perform an action")
+    public void iPerformAnAction() {
+        // TODO: implement
+    }
+
+    @Then("I should see the expected result")
+    public void iShouldSeeAResult() {
+        // TODO: implement
+    }
+}
+
